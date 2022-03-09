21bf0e6e3334f6f83f2ba6ad931ed2429d701728
Derek Pedersen <derekpedersen.com@gmail.com>
2022-03-08 16:28:40 -0800

    got a golang answer to 1.3

--- golang/cracking_the_coding_interview/chapter_one/ArraysStrings.go
+++ golang/cracking_the_coding_interview/chapter_one/ArraysStrings.go
@@ -1,5 +1,7 @@
 package chapter_one
 
+import "sort"
+
 // 1.1
 // implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 func IsUnique(val string) bool {
@@ -9,6 +11,46 @@ func IsUnique(val string) bool {
 			return false
 		}
 		used[v] = 1
+	}
+
+	return true
+}
+
+// 1.2
+// implement a function void reverse(char* str) in C which reverses a null-terminated string
+func ReverseString(val string) string {
+	chars := []rune(val)
+	responsechars := []rune{}
+
+	for i := len(chars) - 1; i >= 0; i-- {
+		responsechars = append(responsechars, chars[i])
+	}
+
+	return string(responsechars)
+}
+
+// 1.3
+// given two strings write a method to decide if one is a permutation of the other
+func ArePermutations(one string, two string) bool {
+	ones := []rune(one)
+	twos := []rune(two)
+
+	if len(ones) != len(twos) {
+		return false
+	}
+
+	sort.Slice(ones, func(i int, j int) bool {
+		return ones[i] < ones[j]
+	})
+
+	sort.Slice(twos, func(i int, j int) bool {
+		return twos[i] < twos[j]
+	})
+
+	for i := 0; i < len(ones); i++ {
+		if ones[i] != twos[i] {
+			return false
+		}
 	}
 
 	return true
--- golang/cracking_the_coding_interview/chapter_one/ArraysStrings_test.go
+++ golang/cracking_the_coding_interview/chapter_one/ArraysStrings_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/derekpedersen/code-samples/golang/cracking_the_coding_interview/chapter_one"
 )
 
+// 1.1
+// implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 func Test_ChapterOne_One(t *testing.T) {
 	testTable := []struct {
 		Val    string
@@ -25,6 +27,64 @@ func Test_ChapterOne_One(t *testing.T) {
 		result := chapter_one.IsUnique(v.Val)
 		if result != v.Unique {
 			t.Errorf("expected %v but got %v for string %v", v.Unique, result, v.Val)
+		}
+	}
+}
+
+// 1.2
+// implement a function void reverse(char* str) in C which reverses a null-terminated string
+func Test_ChapterOne_Two(t *testing.T) {
+	testTable := []struct {
+		Val    string
+		Result string
+	}{
+		{
+			Val:    "asdf",
+			Result: "fdsa",
+		},
+		{
+			Val:    "derek",
+			Result: "kered",
+		},
+	}
+
+	for _, v := range testTable {
+		result := chapter_one.ReverseString(v.Val)
+		if result != v.Result {
+			t.Errorf("expected %v but got %v for string %v", v.Result, result, v.Val)
+		}
+	}
+}
+
+// 1.3
+// given two strings write a method to decide if one is a permutation of the other
+func Test_ChapterOne_Three(t *testing.T) {
+	testTable := []struct {
+		One    string
+		Two    string
+		Result bool
+	}{
+		{
+			One:    "asdf",
+			Two:    "fdsa",
+			Result: true,
+		},
+		{
+			One:    "derek",
+			Two:    "keed",
+			Result: false,
+		},
+		{
+			One:    "fast",
+			Two:    "last",
+			Result: false,
+		},
+	}
+
+	for _, v := range testTable {
+		result := chapter_one.ArePermutations(v.One, v.Two)
+		if result != v.Result {
+			t.Errorf("expected %v but got %v for strings %v and %v", v.Result, result, v.One, v.Two)
 		}
 	}
 }
